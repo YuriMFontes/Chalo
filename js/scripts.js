b7a58711947df3b05b0f72cf62a3fe7f8b59da72
YuriMFontes <yurimedeirosfontes11@gmail.com>
2024-06-12 14:03:50 -0300

    eighteen.2 commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,20 @@
 setInterval(function() {
     document.querySelector('#carouselExampleFade .carousel-control-next').click();
   }, 3000); 
+
+
+  /*olhinho da senha*/
+
+    function togglePassword(inputId, toggleIcon) {
+        const input = document.getElementById(inputId);
+        if (input.type === 'password') {
+            input.type = 'text';
+            toggleIcon.classList.remove('fa-eye');
+            toggleIcon.classList.add('fa-eye-slash');
+        } else {
+            input.type = 'password';
+            toggleIcon.classList.remove('fa-eye-slash');
+            toggleIcon.classList.add('fa-eye');
+        }
+    }
+
